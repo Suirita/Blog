ebd80c759061166d976938faa3bca20dc768c7d0
Suirita Fahd <suirita.fahd.solicode@gmail.com>
2025-05-06 21:41:31 +0100

    save

--- vite.config.ts
+++ vite.config.ts
@@ -7,30 +7,43 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/js/app.ts'],
-            ssr: 'resources/js/ssr.ts',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './resources/js'),
-            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+  plugins: [
+    laravel({
+      input: ['resources/js/app.ts'],
+      ssr: 'resources/js/ssr.ts',
+      // explicitly set these (defaults are the same, but it's clearer):
+      publicDirectory: 'public',
+      buildDirectory: 'build',
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
         },
+      },
+    }),
+  ],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './resources/js'),
+      'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
     },
-    css: {
-        postcss: {
-            plugins: [tailwindcss, autoprefixer],
-        },
+  },
+
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
     },
+  },
+
+  build: {
+    outDir: 'public/build',
+    manifest: true,
+    rollupOptions: {
+      input: 'resources/js/app.ts',
+    },
+  },
 });
